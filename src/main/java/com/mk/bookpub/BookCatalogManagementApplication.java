8bf3707b7cd3e76f028284a9744cfec4666f44c4
mangesh <mbhattilahari@gmail.com>
2023-03-27 20:43:06 +0530

    Initial commit with Project structure

---
+++ src/main/java/com/mk/bookpub/BookCatalogManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mk.bookpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mk/bookpub/BookCatalogManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.bookpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
